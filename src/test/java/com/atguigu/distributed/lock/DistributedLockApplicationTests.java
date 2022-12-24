d48c9567353e50852a44983188f7c4d4a8708ad3
喻适ZIY00097677 <94149094@qq.com>
2022-12-24 09:43:54 +0800

    分布式锁

---
+++ src/test/java/com/atguigu/distributed/lock/DistributedLockApplicationTests.java
@@ -0,0 +1,14 @@
+package com.atguigu.distributed.lock;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/ys/distributed/lock/DistributedLockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.distributed.lock;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
